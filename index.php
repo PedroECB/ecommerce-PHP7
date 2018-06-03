f7a1091a2703788aa76e6ac1ee582103ef4653da
Pedro Bastos <pedroxdman@hotmail.com>
2018-06-03 20:32:09 -0300

    configurado cadastro de usuários

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php 
 
+session_start();
+
 require_once("vendor/autoload.php");
 use \Slim\Slim;
 use Hcode\Page;
 use Hcode\PageAdmin;
+use Hcode\Model\User;
 
 $app = new \Slim\Slim();
 
@@ -20,12 +23,136 @@ $app->get('/', function() {
 
 
 $app->get('/admin', function() {
-    
+
+ User::verifyLogin();
+
   $page = new PageAdmin();
   
   $page->setTpl("index");
 
 });
+
+
+$app->get('/admin/login', function() {
+    
+  $page = new PageAdmin([
+    "header"=>false,
+    "footer"=>false
+  ]);
+  
+  $page->setTpl("login");
+
+});
+
+
+$app->post('/admin/login', function() {
+    
+  User::login($_POST['login'], $_POST['senha']);
+
+  header("Location: /admin");
+  exit;
+
+});
+
+$app->get('/admin/logout', function(){
+ 
+  User::logout();
+  header("Location: /admin/login");
+  exit;
+
+});
+
+
+
+
+$app->get('/admin/users', function(){
+    
+    User::verifyLogin();                            #Lista usuários
+    $users = User::listAll();
+
+
+    $page = new PageAdmin();
+    $page->setTpl("users", array(
+      "users"=>$users
+    ));
+
+
+});
+                                                        // CADASTRO DE USUÁRIOS
+
+$app->get('/admin/users/create', function(){
+    
+    User::verifyLogin();                                #GET Cadastro usuários
+    
+    $page = new PageAdmin();
+    $page->setTpl("users-create");
+
+
+});
+
+
+
+
+$app->post('/admin/users/create', function(){
+    
+    User::verifyLogin();                            #POST Cadastro de usuários
+    
+
+   $user = new User();
+   $user->setData($_POST);
+   $user->save();
+ 
+   header("Location:/admin/users");
+   exit;
+
+});
+
+                                                          //  EDIÇÃO DE USUÁRIO
+
+
+
+
+$app->get('/admin/users/:iduser/delete', function($iduser){
+    
+    User::verifyLogin();                                  #Deletar
+    
+    $page = new PageAdmin();
+    $page->setTpl("users-update");
+
+
+});
+
+
+
+
+
+$app->get('/admin/users/:iduser', function($iduser){
+    
+    User::verifyLogin();   
+    $user = new User();
+    $user->get((int)$iduser);                                   #Visualiza para edição
+
+    $page = new PageAdmin();
+    $page->setTpl("users-update", array("user"=>$user->getValues()));
+
+
+});
+
+
+
+
+
+$app->post('/admin/users/:iduser', function($iduser){
+    
+    User::verifyLogin();
+    
+    $page = new PageAdmin();                        #Atualizar
+    $page->setTpl("users-update");
+
+
+});
+
+
 
 
 
---
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -0,0 +1,98 @@
+<?php 
+
+namespace Hcode\Model;
+use \Hcode\DB\Sql;
+use \Hcode\Model;
+
+class User extends Model{
+
+ const SESSION = "User";
+
+  public static function login($login, $senha){
+    $sql = new Sql();
+    $results = $sql->select("SELECT * FROM tb_users WHERE deslogin=:LOGIN", array(
+      ":LOGIN"=>$login
+    ));
+
+    if(count($results) === 0){
+
+      throw new \Exception('Usuário inexistente ou senha inválida 1');
+
+    }
+
+    $data = $results[0];
+
+    if(password_verify($senha, $data['despassword'])){
+
+       $user = new User();
+
+       $user->setData($data);
+
+       $_SESSION[User::SESSION] = $user->getValues();
+
+       return $user;
+
+
+
+    }else{
+      throw new Exception('Usuário inexistente ou senha incorreta 2');
+    }
+
+
+  }
+
+
+public static function verifyLogin($inadmin = true){
+
+  if(!isset($_SESSION[User::SESSION])
+   || !$_SESSION[User::SESSION]
+   || !(int)$_SESSION[User::SESSION]["iduser"] > 0
+   || (bool)$_SESSION[User::SESSION]["inadmin"] !== $inadmin
+ ){
+
+      header("Location: /admin/login");
+      exit;
+  }
+}
+
+
+public static function logout(){
+  $_SESSION[User::SESSION] = NULL;
+}
+
+public static function listAll(){
+
+  $sql = new Sql();
+  return $sql->select("SELECT * FROM tb_users  a INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson");
+}
+
+
+
+public function save(){
+
+    $sql = new Sql();
+    $result = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+      ":desperson"=>$this->getdesperson(),
+      ":deslogin"=>$this->getdeslogin(),
+      ":despassword"=>$this->getdespassword(),
+      ":desemail"=>$this->getdesemail(),
+      ":nrphone"=>$this->getnrphone(),
+      ":inadmin"=>$this->getinadmin()
+    ));
+
+ $this->setData($result[0]);
+
+
+}
+
+
+public static function get($iduser){
+  $sql = new Sql();
+  $result = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = :iduser"
+    , array(":iduser"=>$iduser));
+
+  $this->setData($results[0]);
+}
+
+
+}
