42dbe4e44f6aad8fba3168a895e9a8b9db7dc7f5
Pedro Bastos <pedroxdman@hotmail.com>
2018-07-10 14:51:20 -0300

    carrinho e calculo de frete

--- site.php
+++ site.php
@@ -72,9 +72,12 @@ $cart = Cart::getFromSession();
 
 
 $page = new Page();
+
+
 $page->setTpl("cart", [
   'cart'=>$cart->getValues(),
-  'products'=>$cart->getProducts()
+  'products'=>$cart->getProducts(),
+  'error'=>Cart::getMsgError()
 ]);
 
 
@@ -135,6 +138,20 @@ $app->get("/cart/:idproduct/remove", function($idproduct){
 
   header("Location: /cart");
   exit;
+
+
+});
+
+
+$app->post("/cart/freight", function(){
+
+  $cart = Cart::getFromSession();
+  $cart->setFreight($_POST['zipcode']);
+  header("Location: /cart");
+  exit;
+
+
+
 
 
 });
